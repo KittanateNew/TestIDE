17e56aaba2a932ce11a888fffdad20a30b3b018f
JhopperJH <jumphopper.jh@gmail.com>
2022-04-05 15:01:40 +0700

    Update Test.java

--- Test.java
+++ Test.java
@@ -2,5 +2,6 @@ public class Test {
     public static void main(String[] args){
         System.out.print("Hello world");
         System.out.print("Hello New");
+        System.out.print("Bye Bye");
     }
 }
